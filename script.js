07b006842b899fe44007152d77855fb09e11d5cf
albinasaveleva <savelevalbina@gmail.com>
2021-08-24 19:49:20 +0300

    add removeTransition function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let removeTransition = () => {
+    document.querySelectorAll('.key').forEach(key => {
+        key.addEventListener('transitionend', (event) => {
+            if (event.propertyName.toLowerCase() === 'transform') {
+                key.classList.remove('playing');
+            }
+        }) 
+    })
+};
 let playSound = (event) => {
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     if (key) {
@@ -5,6 +14,7 @@ let playSound = (event) => {
         const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
         audio.currentTime = 0;
         audio.play();
+        setTimeout(removeTransition, .07)
     }
 };
 
